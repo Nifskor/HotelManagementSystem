15204dd71913efc8ef841d646b5407a38044ed24
Nifskor <nifskr@naver.com>
2021-11-19 17:35:09 +0900

    first commit5

--- HotelmanegementSystem/src/main/java/dong/hotel/check/login.java
+++ HotelmanegementSystem/src/main/java/dong/hotel/check/login.java
@@ -10,6 +10,7 @@ package dong.hotel.check;
  */
 public class login {
     public static void main (String[] args){
-    System.out.println("tsesdddddfdfffdfddfdfst");
+    System.out.println("tsesdddddfddfsdfadffffdfddfdfst");
+   
     }    
 }
