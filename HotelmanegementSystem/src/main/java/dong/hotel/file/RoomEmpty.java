f72d088d6cf9bdd8f80a272c322ff88909218a32
hj592 <92974916+hj592@users.noreply.github.com>
2021-11-24 03:15:48 +0900

    RoomEmpty클래스 파일처리

--- HotelmanegementSystem/src/main/java/dong/hotel/file/RoomEmpty.java
+++ HotelmanegementSystem/src/main/java/dong/hotel/file/RoomEmpty.java
@@ -4,10 +4,48 @@
  */
 package dong.hotel.file;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author nifskorea
  */
 public class RoomEmpty {
-    
+
+    public void Outguest(String indexNum, String roomNum) throws IOException {
+        File file = new File("C:\\DB\\roomState.txt");
+        String newLine = "";
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            String line;
+
+            for (int i = 0; i < Integer.parseInt(indexNum) - 1; i++) {
+                line = br.readLine();
+                newLine += (line + "\r\n");
+            }
+
+            br.readLine();
+            newLine += (indexNum + " " + roomNum + " empty booker guestNum inDate inTime  realInTime outDate outTime\r\n");
+
+            while ((line = br.readLine()) != null) {
+                newLine += (line + "\r\n");
+            }
+
+            FileWriter fw = new FileWriter("C:\\DB\\roomState.txt");
+
+            fw.write(newLine);
+            fw.flush();
+            fw.close();
+
+            br.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ HotelmanegementSystem/src/main/java/dong/hotel/file/Test.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dong.hotel.file;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author heehe
+ */
+public class Test {
+
+    /**
+     * 파일돌려보는 테스트용코드
+     */
+    public static void main(String[] args) throws IOException {
+       /*
+       //파일에 빈방싹넣음
+        RoomEmpty a = new RoomEmpty();
+        int i, no;
+        int height;
+        int room;
+        for (i = 1, room = 101; i < 101 && room < 1011; i++, room++) {
+            if (room % 100 == 11) {
+                room = room + 90;
+            }
+            String index = Integer.toString(i);
+            String num = Integer.toString(room);
+
+            a.Outguest(index, num);
+        }*/
+    }
+}
