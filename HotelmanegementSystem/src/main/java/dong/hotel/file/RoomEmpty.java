e3e9379c25212595f993484c3d3f31c57b86036e
Nifskor <nifskr@naver.com>
2021-11-26 02:05:04 +0900

    roomempty, rommstate 환경변수 추가함

--- HotelmanegementSystem/src/main/java/dong/hotel/file/RoomEmpty.java
+++ HotelmanegementSystem/src/main/java/dong/hotel/file/RoomEmpty.java
@@ -5,6 +5,7 @@
 package dong.hotel.file;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -16,13 +17,17 @@ import java.io.InputStreamReader;
  * @author nifskorea
  */
 public class RoomEmpty {
-
+ /* 개발 환경 차이로인해 사전 환경 경로 지정 */ 
+private String macosxroomstate = "/Users/nifskorea/Desktop/DB/roomState.txt";  // 
+private String windowsroomstate = "C:\\DB\\roomState.txt";
+ private String line ="";  // 파일 공백시 데이터를 채워줌  
+ SFR100Process fileae = new SFR100Process();
     public void outguest(String indexNum, String roomNum) throws IOException {
-        File file = new File("C:\\DB\\roomState.txt");
         String newLine = "";
+        if(fileae.getOssystem() == 1) {
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            String line;
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(macosxroomstate)));
+            
 
             for (int i = 0; i < Integer.parseInt(indexNum) - 1; i++) {
                 line = br.readLine();
@@ -36,7 +41,34 @@ public class RoomEmpty {
                 newLine += (line + "\r\n");
             }
 
-            FileWriter fw = new FileWriter("C:\\DB\\roomState.txt");
+            BufferedWriter fw = new BufferedWriter(new FileWriter(macosxroomstate, false));
+            fw.write(newLine);
+            
+            fw.close();
+            br.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        } else if(fileae.getOssystem() == 2) {
+        
+              try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(windowsroomstate)));
+        
+
+            for (int i = 0; i < Integer.parseInt(indexNum) - 1; i++) {
+                line = br.readLine();
+                newLine += (line + "\r\n");
+            }
+
+            br.readLine();
+            newLine += (indexNum + " " + roomNum + " empty booker guestNum inDate inTime  realInTime outDate outTime\r\n");
+
+            while ((line = br.readLine()) != null) {
+                newLine += (line + "\r\n");
+            }
+
+             BufferedWriter fw = new BufferedWriter(new FileWriter(windowsroomstate, false));
 
             fw.write(newLine);
             
@@ -46,5 +78,7 @@ public class RoomEmpty {
         } catch (Exception e) {
             e.printStackTrace();
         }
+              
+    }
     }
 }
--- HotelmanegementSystem/src/main/java/dong/hotel/file/RoomStateSave.java
+++ HotelmanegementSystem/src/main/java/dong/hotel/file/RoomStateSave.java
@@ -5,6 +5,7 @@
 package dong.hotel.file;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -17,13 +18,18 @@ import java.io.InputStreamReader;
  */
 /* 환경변수 추가해야함 */ 
 public class RoomStateSave {
+             /* 개발 환경 차이로인해 사전 환경 경로 지정 */ 
+private String macosxroomstata = "/Users/nifskorea/Desktop/DB/roomState.txt";  // 
+private String windowsroomstate = "C:\\DB\\roomState.txt";
+ private String line ="";  // 파일 공백시 데이터를 채워줌  
+ SFR100Process fileae = new SFR100Process();
+ 
      public void inguest(String indexNum, String roomNum, String name,String guest,String inDate, String time, String outDate) throws IOException {
-        File file = new File("C:\\DB\\roomState.txt");
+      if(fileae.getOssystem() ==1 ){
         String newLine = "";
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            String line;
-
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(macosxroomstata)));
+           
             for (int i = 0; i < Integer.parseInt(indexNum) - 1; i++) {
                 line = br.readLine();
                 newLine += (line + "\r\n");
@@ -36,8 +42,8 @@ public class RoomStateSave {
                 newLine += (line + "\r\n");
             }
 
-            FileWriter fw = new FileWriter("C:\\DB\\roomState.txt");
-
+            
+            BufferedWriter fw = new BufferedWriter(new FileWriter(macosxroomstata, false));
             fw.write(newLine);
             
             fw.close();
@@ -46,5 +52,34 @@ public class RoomStateSave {
         } catch (Exception e) {
             e.printStackTrace();
         }
+      } 
+      else if (fileae.getOssystem() ==2 ){
+           String newLine = "";
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(windowsroomstate)));
+           
+            for (int i = 0; i < Integer.parseInt(indexNum) - 1; i++) {
+                line = br.readLine();
+                newLine += (line + "\r\n");
+            }
+
+            br.readLine();
+            newLine += (indexNum + " " + roomNum + " full "+name+" "+guest+" "+inDate+" 15:00 "+time+" "+outDate+" 11:00\r\n");
+
+            while ((line = br.readLine()) != null) {
+                newLine += (line + "\r\n");
+            }
+
+            
+            BufferedWriter fw = new BufferedWriter(new FileWriter(windowsroomstate, false));
+            fw.write(newLine);
+            
+            fw.close();
+            br.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      }
      }
 }
